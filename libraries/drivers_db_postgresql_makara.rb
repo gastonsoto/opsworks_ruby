0c0c416eb2bc1c03a88a1a29b1f3381ff09221a3
Gaston Soto <gallzeus@gmail.com>
2018-05-22 12:18:32 -0300

    postgresql_makara addition

---
+++ libraries/drivers_db_postgresql_makara.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Drivers
+  module Db
+    class PostgresqlMakara < Base
+      adapter :postgresql_makara
+      allowed_engines :postgres, :postgresql
+      packages debian: 'libpq-dev', rhel: 'postgresql96-devel'
+    end
+  end
+end
